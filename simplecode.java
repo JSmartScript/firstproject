ff936d5902b3551759079eb4eeb610dea2e62b35
JSmartScript <141119786+JSmartScript@users.noreply.github.com>
2023-08-01 16:13:40 +0530

    Update simplecode.java
    simple code change

--- simplecode.java
+++ simplecode.java
@@ -1,5 +1,8 @@
 public class simplecode {
     public static void main (String args[]) {
         System.out.println("Hello World");
+        int a = 1;
+        int b = a+1;
+        System.out.println(b);
     }
 }
